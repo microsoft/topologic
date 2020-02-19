808e38f20ce430c2ec2103c635c0cb85a0f21b38
Dwayne Pryce <dwpryce@microsoft.com>
2020-02-19 15:39:53 -0800

    Double imported the clustering and metric packages

--- topologic/embedding/__init__.py
+++ topologic/embedding/__init__.py
@@ -6,6 +6,7 @@ from . import metric
 from .adjacency_spectral_embedding import adjacency_embedding
 from .elbow_finder import find_elbows
 from .embedding_container import EmbeddingContainer, OutOfSampleEmbeddingContainer
+from . import distance
 from .embedding_methods import EmbeddingMethod
 from .laplacian_spectral_embedding import laplacian_embedding
 from .node2vec_embedding import node2vec_embedding
@@ -13,10 +14,6 @@ from .omnibus_embedding import omnibus_embedding, generate_omnibus_matrix
 from .pca import pca
 from .sample_methods import SampleMethod, sample_graph_by_edge_weight, sample_graph_by_vertex_degree
 from .tsne import tsne
-
-from . import clustering
-from . import metric
-from . import distance
 
 __all__ = [
     'EmbeddingContainer',
