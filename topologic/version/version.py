c6de2453599a8f73a19136448fdead66d7c08293
Nick <nyecarr@gmail.com>
2020-11-12 08:00:59 -0800

    Update release notes and version to 0.1.6

--- topologic/version/version.py
+++ topologic/version/version.py
@@ -10,7 +10,7 @@ __all__: List[str] = ["version", "name"]
 name = "topologic"
 
 # manually updated
-__semver = "0.1.5"
+__semver = "0.1.6"
 #  full version (may be same as __semver on release)
 __version_file = "version.txt"
 
